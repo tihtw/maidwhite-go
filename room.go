e030477951fb70dd24ef3490ccfc6a7380ab1eb1
Pichu Chen <pichubaby@gmail.com>
2018-11-02 23:52:23 +0800

    add home and room

---
+++ room.go
@@ -0,0 +1,19 @@
+package maidwhiteclient
+
+import (
+// "encoding/json"
+// "fmt"
+// "io/ioutil"
+// "log"
+// "net/http/httputil"
+// "net/url"
+// "strings"
+)
+
+type Room struct {
+	Id          int    `json:"id,omitempty"`
+	DisplayName string `json:"display_name,omitempty"`
+	// DisplayPicture string `json:"display_picture,omitempty"`
+	// RoomId         int    `json:"room_id,omitempty"`
+	// DriverName     string `json:"driver_name,omitempty"`
+}
